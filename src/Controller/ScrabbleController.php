21047879c59dd8f3006289a28909595707c2683f
Félix Bardy <felix@bardy.fr>
2022-06-01 18:07:03 +0200

    Extended the ScrabbleController

--- src/Controller/ScrabbleController.php
+++ src/Controller/ScrabbleController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\InputList;
 use App\Form\WordSubmissionFormType;
+use App\GameGenerator;
+use App\Repository\InputListRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,26 +16,89 @@ use Twig\Environment;
 class ScrabbleController extends AbstractController
 {
     public function __construct(
-        private Environment $twig
+        private Environment $twig,
+        private InputListRepository $inputListRepository,
+        private GameGenerator $gameGenerator
     )
     {}
 
     #[Route('/scrabble', name: 'scrabble')]
     public function index(Request $request): Response
     {
-        $input = "abcefgjki";
+        $input = '';
+        // On récupère le dernier set de lettre de la base de données
+        // (celui d'aujourd'hui)
+        $inputList = $this->inputListRepository
+                      ->findOneBy([], ['id' => 'DESC'])
+        ;
+        
+        // S'il n'existe pas, on en génère un
+        if (null === $inputList) {
+            $inputList = $this->gameGenerator->generateGame(7);
+            $this->inputListRepository->add($inputList, true);
+        }
 
+        // On récupère le set de lettre préédent
+        $previousInputList = $this->inputListRepository
+                                  ->find($inputList->getId()-1)
+        ;
+
+        $words = [];
+        $previousInput = '';
+        // S'il n'existe pas, le set de mots affiché sera vide
+        if (null !== $previousInputList) {
+            $previousInput = $previousInputList->getInputList();
+            $words = $this->gameGenerator->generateSolution($previousInput);
+        }
+            
+        // On récupère le set de lettres
+        $input = $inputList->getInputList();
+
+        // On récupère la session
+        $session = $request->getSession();
+        // Si la session n'existe pas, on la démarre
+        if (!$session->isStarted()) {
+            $session->start();
+        }
+    
+        // Si le set de lettres a changé, vider les tentatives
+        if ($input !== $session->get('scrabble_input', '')) {
+            $session->set('tries', []);
+            $session->set('scrabble_input', $input);
+        }
+
+        // On récupère la liste des tentatives
+        $tries = $session->get('tries', []);
+
+        // On récupère le formulaire
         $form = $this->createForm(WordSubmissionFormType::class);
-
         $form->handleRequest($request);
 
-        $word = $form->getData()['word'];
+        // Si le formlaire est rempli et valide, on récupère le mot donné
+        if ($form->isSubmitted() && $form->isValid()) {
+            $word = $form->getData()['word'];
+            // Si on a pas encore enregistré d'essais, initialiser le champ
+            if (null === $session->get('tries')) {
+                $session->set('tries', [
+                    [$word, $this->gameGenerator->computeScore($word,$input)]
+                ]);
+            }
+            // Sinon, ajouter notre essai et trier le tableau
+            else {
+                $tries[] = [$word, $this->gameGenerator->computeScore($word,$input)];
+                usort($tries, fn ($try1, $try2) => $try2[1] - $try1[1]);
+                $session->set('tries', $tries);
+            }
+        }
 
         $response = new Response(
             $this->twig->render('scrabble/index.html.twig',
             [
                 'input' => str_split($input),
-                'submit_word_form' => $form->createView()
+                'submit_word_form' => $form->createView(),
+                'tries' => $tries,
+                'previous_input' => str_split($previousInput),
+                'previous_words' => $words
             ])
         );
 
