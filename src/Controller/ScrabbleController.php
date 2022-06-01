25491c71b5e2fa81d170c2e564aac7dd33af18f2
Félix Bardy <felix@bardy.fr>
2022-06-01 15:15:52 +0200

    Ajout du formulaire d'envoi de mot

--- src/Controller/ScrabbleController.php
+++ src/Controller/ScrabbleController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\WordSubmissionFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -15,18 +17,30 @@ class ScrabbleController extends AbstractController
     {}
 
     #[Route('/scrabble', name: 'scrabble')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $input = "abcefgjki";
+
+        $form = $this->createForm(WordSubmissionFormType::class);
+
+        $form->handleRequest($request);
+
+        $word = $form->getData()['word'];
 
         $response = new Response(
             $this->twig->render('scrabble/index.html.twig',
             [
                 'input' => str_split($input),
-                'submit_word_form' => "Un magnifique formulaire que voilà"
+                'submit_word_form' => $form->createView()
             ])
         );
 
         return $response;
+    }
+
+    private function checkWordValidity(string $word): bool
+    {
+        //TODO Vérifier la validité à l'aide du set de lettres dans la bdd
+        return true;
     }
 }
